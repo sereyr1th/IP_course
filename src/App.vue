34f47cb60941a68e99839264bcdc4687851c3dba
Rith <mobshigeo99@gmail.com>
2024-10-23 10:32:38 +0700

    TP01: 4 steps guide from setting up the project to basic debugging

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  This is my first VueJs project
+
+  <RouterView />
+</template>
+
+<style scoped></style>
